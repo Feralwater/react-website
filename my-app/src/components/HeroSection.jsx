fb51c21bf516183d3ad62dcdbcd5e7818af70aa6
User <2176589@mail.ru>
2021-07-04 23:07:11 -0700

    Add home page

---
+++ my-app/src/components/HeroSection.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Button} from "./Button";
+import '../App.css'
+import './HeroSection.scss'
+import {faPlayCircle} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+function HeroSection() {
+  const playCircle = <FontAwesomeIcon icon={faPlayCircle}/>
+  return (
+    <div className='hero-container'>
+      <video src='/videos/video-1.mp4' autoPlay loop muted />
+      <h1>ADVENTURE AWAITS</h1>
+      <p>What are you waiting for?</p>
+      <div className='hero-btns'>
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+          GET STARTED
+        </Button>
+        <Button
+          className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
+        >
+          WATCH TRAILER {playCircle}
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
